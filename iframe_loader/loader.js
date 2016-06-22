f5e4a194115da1cba1e83cf866febe896b59d9fe
Ori Newman <orinewman1@gmail.com>
2016-06-22 15:43:41 +0300

    angular bootstrap

--- iframe_loader/loader.js
+++ iframe_loader/loader.js
@@ -1,4 +1,37 @@
-function loadIframe(dataUrl){
-	var iframe = document.createElement('iframe');
-	iframe.src = 'http://pixter.local/';
-}
+(function(){
+	var imgDataUrl,iframe;
+	window.pixter = {
+		loadIframe:loadIframe,
+		changeImage: changeImage,
+	};
+
+	window.addEventListener('message', function(e) {
+		if( e.data === 'pixter_init' ){
+			postImage();
+		}
+	}, false);
+
+	function loadIframe(dataUrl){
+		if( !iframe ){
+			iframe = document.createElement('iframe');
+			iframe.src = '../';
+			iframe.style.position = 'fixed';
+			iframe.style.left = '20vw';
+			iframe.style.top = '20vh';
+			iframe.style.width = '60vw';
+			iframe.style.height = '60vh';
+		}
+		iframe.style.display = 'block';
+		document.body.appendChild(iframe);
+		changeImage(dataUrl);
+	}
+
+	function changeImage(dataUrl){
+		imgDataUrl = dataUrl;
+		postImage();
+	}
+
+	function postImage(){
+		iframe.contentWindow.postMessage(imgDataUrl, '*');
+	}
+})();
