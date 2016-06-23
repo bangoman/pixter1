11bbf09b886493d17d9cabc474cec3bf32f08d84
Ori Newman <orinewman1@gmail.com>
2016-06-23 10:43:38 +0300

    default image

--- app/js/run/messaging.js
+++ app/js/run/messaging.js
@@ -2,7 +2,7 @@ angular.module('app').run(function($rootScope,message){
 	$rootScope.sourceImg = '/app/images/image1.jpg';
 	message('init');
     window.addEventListener('message', function(e) {
-        if (e.data) {
+        if (e.source !== window && e.data) {
             $rootScope.$apply(function(){
                 $rootScope.sourceImg = e.data;
             });
