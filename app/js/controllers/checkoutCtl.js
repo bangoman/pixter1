978e1c9302dc9e86da8179f8e8a0302c5e83776c
Ben noach <bangoman@gmail.com>
2016-06-23 17:37:07 +0300

    checkout

--- app/js/controllers/checkoutCtl.js
+++ app/js/controllers/checkoutCtl.js
@@ -1,3 +1,13 @@
-angular.module('app').controller('checkoutCtl',function(){
+angular.module('app').controller('checkoutCtl',function($uibModal){
     var vm = this;
+
+    vm.shipmentMethod = 'standart';
+
+    vm.openCuponModel = function(){
+        $uibModal.open({
+            templateUrl: 'app/views/cupon_model.html',
+            controller: 'cuponModelCtl as vm',
+            backdrop:'static',
+        });
+    }
 });
