a6a19692c3c3f52010d4fe85b828e1528db48352
Ben noach <bangoman@gmail.com>
2016-06-27 13:27:50 +0300

    products

--- app/js/controllers/shopCtl.js
+++ app/js/controllers/shopCtl.js
@@ -1,11 +1,16 @@
 angular.module('app').controller('shopCtl',function($state, $http){
 	var vm = this;
-      $http.get('app/json/products.json')
-        .then(function(res){
-          vm.products = res.data.products;                
-        });
-        console.log('vm.products = ',vm.products);
-
+    
+    vm.getProductsData = function(){
+        $http.get('app/json/products.json')
+            .then(function(res){
+                vm.productsData = res.data;                
+            }).then(function(){
+                console.log('vm.productsData = ',vm.productsData);
+            });
+    }
+    vm.getProductsData();
+        
 
     vm.goToPreview = function () {
         $state.go('app.preview');
