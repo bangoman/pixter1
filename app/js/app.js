1c827ddfb5ef86a0dfc180e1b81ad0fe157a5c12
Ben Noach <bangoman@Bens-iMac.local>
2016-06-27 12:06:35 +0300

    ds store

--- app/js/app.js
+++ app/js/app.js
@@ -4,4 +4,5 @@ var app = angular.module('app',[
     'ui.bootstrap',
     'angular-carousel',
     'ngDraggable',
+    'ionic',
 ]);
--- app/js/directives/productImageDisplay.js
+++ app/js/directives/productImageDisplay.js
@@ -7,70 +7,76 @@ angular.module('app').directive('productImageDisplay', function ($http) {
 	    restrict: 'E',
 	  	controller: ['$scope','$http','$attrs','$document','$element','$compile', function($scope,$http,$attrs,$document,$element,$compile) {
 
-		function getImgSize(imgSrc) {
-		    var newImg = new Image();
+		    $scope.onDrag=function(event){
+		       console.log("deltaY", event.gesture.deltaY);
+		       
+		    }	  		
 
-		    newImg.onload = function() {
-		      var height = newImg.height;
-		      var width = newImg.width;
-		      
-		      calculatePosition(newImg);
-		      //alert ('The image size is '+width+'*'+height);
-		    }
+			function getImgSize(imgSrc) {
+			    var newImg = new Image();
 
-		    newImg.src = imgSrc; // this must be done AFTER setting onload
-		}
+			    newImg.onload = function() {
+			      var height = newImg.height;
+			      var width = newImg.width;
+			      
+			      calculatePosition(newImg);
+			      //alert ('The image size is '+width+'*'+height);
+			    }
 
-		function calculatePosition(img){
-			//var imgDisplay = document.getElementById("img");
-			var imgRatio = img.height/img.width;
-			
+			    newImg.src = imgSrc; // this must be done AFTER setting onload
+			}
 
-			var productRatio = $scope.product.window.h/$scope.product.window.w;
+			function calculatePosition(img){
+				//var imgDisplay = document.getElementById("img");
+				var imgRatio = img.height/img.width;
+				
 
-			$scope.product.tmbWidth = parseInt($attrs.tmbwidth);
-			$scope.product.tmbHeight = $scope.product.tmbWidth * ($scope.product.height/$scope.product.width) ;
-			$scope.sizeRatio = $scope.product.width/$scope.product.tmbWidth;			
+				var productRatio = $scope.product.window.h/$scope.product.window.w;
 
-			if(productRatio >= imgRatio){
-				$scope.imageSizeRatio = img.height/($scope.product.window.h/ $scope.sizeRatio) ;			
-				var tmp =($scope.product.window.x/$scope.sizeRatio) -  (((img.width/$scope.imageSizeRatio) - ($scope.product.window.w/$scope.sizeRatio))/2 )     + "px ";
-				$scope.imageStyle = {					
-					"background-image":"url('" + $attrs.imageurl  + "')",
-					"background-size": "auto "  + $scope.product.window.h / $scope.sizeRatio + "px",
-					"background-repeat":"no-repeat", 
-					"background-position": tmp + $scope.product.window.y / $scope.sizeRatio + "px",
-					"width":$attrs.tmbwidth + "px"
+				$scope.product.tmbWidth = parseInt($attrs.tmbwidth);
+				$scope.product.tmbHeight = $scope.product.tmbWidth * ($scope.product.height/$scope.product.width) ;
+				$scope.sizeRatio = $scope.product.width/$scope.product.tmbWidth;			
+				$scope.backgroundPosition = {top:"",left:""}								
+				if(productRatio >= imgRatio){ // Left case
+					$scope.imageSizeRatio = img.height/($scope.product.window.h/ $scope.sizeRatio) ;			
+
+					$scope.backgroundPosition.left =($scope.product.window.x/$scope.sizeRatio) -  (((img.width/$scope.imageSizeRatio) - ($scope.product.window.w/$scope.sizeRatio))/2 )     + "px ";
+					$scope.backgroundPosition.top =  $scope.product.window.y / $scope.sizeRatio + "px";
+
+					$scope.imageStyle = {					
+						"background-image":"url('" + $attrs.imageurl  + "')",
+						"background-size": "auto "  + $scope.product.window.h / $scope.sizeRatio + "px",
+						"background-repeat":"no-repeat", 
+						"background-position": $scope.backgroundPosition.left + $scope.backgroundPosition.top,
+						"width":$attrs.tmbwidth + "px"
+
+					}
+					console.log((img.width/$scope.imageSizeRatio) ,  $scope.product.window.w/$scope.sizeRatio);
+					$scope.$apply();				
+				}else{ // top case
+					$scope.imageSizeRatio = img.width/($scope.product.window.w/ $scope.sizeRatio) ;		
+					$scope.backgroundPosition.top = ($scope.product.window.y/$scope.sizeRatio) - (((img.height/$scope.imageSizeRatio) - ($scope.product.window.h/$scope.sizeRatio))/2 ) + "px "	
+					$scope.backgroundPosition.left = $scope.product.window.x / $scope.sizeRatio + "px ";
+					$scope.imageStyle = {					
+						"background-image":"url('" + $attrs.imageurl  + "')",
+						"background-size": $scope.product.window.w / $scope.sizeRatio + "px" + " auto",
+						"background-repeat":"no-repeat", 
+						"background-position": $scope.backgroundPosition.left + $scope.backgroundPosition.top,
+						"width":$attrs.tmbwidth + "px"
+
+					}
+					$scope.$apply();
+
 
 				}
-				console.log((img.width/$scope.imageSizeRatio) ,  $scope.product.window.w/$scope.sizeRatio);
-				$scope.$apply();				
-			}else{
-				$scope.imageSizeRatio = img.width/($scope.product.window.w/ $scope.sizeRatio) ;		
-				var tmp = ($scope.product.window.y/$scope.sizeRatio) - (((img.height/$scope.imageSizeRatio) - ($scope.product.window.h/$scope.sizeRatio))/2 ) + "px "	
-				$scope.imageStyle = {					
-					"background-image":"url('" + $attrs.imageurl  + "')",
-					"background-size": $scope.product.window.w / $scope.sizeRatio + "px" + " auto",
-					"background-repeat":"no-repeat", 
-					"background-position": $scope.product.window.x / $scope.sizeRatio + "px " + tmp,
-					"width":$attrs.tmbwidth + "px"
-
-				}
-				$scope.$apply();
+				
 
 
 			}
-			
-
-
-		}
-	  	
-	  	//$scope.imageStyle={"width":"500px","height":"400px","background":"pink"}
-	  	$scope.product = {"type":"mug","window":{"w":200,"h":200,"x":175,"y":107},"width":600,"height":360,"shortName":"Mug","marketingName":"11oz White Mug","teaser":true,"cropRatio":0.75,"previewImage":"mug/previewImage.png","categoryText":"Ceramic 11oz MUG","children":["CMUG11OZ111MUG"],"index":1};
-	  	getImgSize($attrs.imageurl);
-	    $scope.onDragComplete=function(data,evt){
-	       console.log("drag success, data:", data);
-	    }	  		
+		  	
+		  	//$scope.imageStyle={"width":"500px","height":"400px","background":"pink"}
+		  	$scope.product = {"type":"mug","window":{"w":200,"h":200,"x":175,"y":107},"width":600,"height":360,"shortName":"Mug","marketingName":"11oz White Mug","teaser":true,"cropRatio":0.75,"previewImage":"mug/previewImage.png","categoryText":"Ceramic 11oz MUG","children":["CMUG11OZ111MUG"],"index":1};
+		  	getImgSize($attrs.imageurl);
 
 	  	
 	    }],    
