b90ec754b99ad6f58d95e6ce9be8210b47002e2a
Ben Noach <bangoman@Bens-iMac.local>
2016-07-05 17:35:26 +0300

    reality

--- app/js/controllers/editCtl.js
+++ app/js/controllers/editCtl.js
@@ -1,7 +1,11 @@
-angular.module('app').controller('editCtl',function($scope){
+angular.module('app').controller('editCtl',function($scope,$rootScope){
     var vm = this;
     $scope.zoom = 1;
     $scope.zoomFactor = 0.2;
+    $scope.tmbWidth = $rootScope.screenW*0.92;
+    if($scope.tmbWidth > 520){
+    	$scope.tmbWidth = 520;
+    }
     $scope.initZoom = function(value){
     	$scope.zoom += value
     	//$scope.$apply();
