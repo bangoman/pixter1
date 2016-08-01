2962f461a75c10d2d327619b5e3d112112dc1261
Ben Noach <bangoman@bens-imac.mynet>
2016-08-01 09:32:34 +0300

    .

--- app/js/directives/productParams.js
+++ app/js/directives/productParams.js
@@ -10,13 +10,16 @@ angular.module('app').directive('productParams', function () {
 			if($scope.params){
 				generateBackgrounds();
 			}
+			
 			function generateBackgrounds(){
 				for (var i = $scope.params.length - 1; i >= 0; i--) {
 					if($scope.params[i].key == "background"){
 						$scope.params[i].options = $rootScope.bgs
+
 					}
 				}				
 			}
+			$rootScope.choosenParams.backSideColor = $rootScope.bgs[0];
 			$scope.setBackground = function(option){
 				$rootScope.choosenParams.backSideColor = option;
 				
