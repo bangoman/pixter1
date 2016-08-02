e095d1dfdf8cdcef7d6782c39fc8fde38e54943b
Ben Noach <bangoman@bens-imac.mynet>
2016-08-02 15:12:21 +0300

    .

--- app/js/directives/productParams.js
+++ app/js/directives/productParams.js
@@ -30,10 +30,13 @@ angular.module('app').directive('productParams', function () {
 			$scope.setBackground = function(option){
 				$rootScope.choosenParams.backSideColor = option;
 			}
+			function getQantity(){
+				
+			}
 			$scope.setPrice = function(){
 				$scope.price = 0;
 				for (var i = $scope.params.length - 1; i >= 0; i--) {
-					if($scope.params[i].key != "background" && $scope.params[i].chosenOption.quantity){
+					if($scope.params[i].key != "background" && $scope.params[i].chosenOption.quantity &&){
 						var quantity = $scope.params[i].chosenOption.quantity;
 						for (var i = $scope.params.length - 1; i >= 0; i--) {
 							if($scope.params[i].key != "background" && $scope.params[i].key != "quantity"){
@@ -54,11 +57,7 @@ angular.module('app').directive('productParams', function () {
 				for (var i = $scope.params.length - 1; i >= 0; i--) {
 					for (var j = $scope.params[i].options.length - 1; j >= 0; j--) {
 						if($scope.params[i].options[j].default){
-<<<<<<< HEAD
-							//$scope.params[i].chosenOption = $scope.params[i].options[j];
-=======
 							$scope.params[i].chosenOption = $scope.params[i].options[j];
->>>>>>> 3f26d5273ffbd42b1c308a47f8a2a76516f13308
 						}						
 					}
 				}
