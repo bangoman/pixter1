51b967c44cff02397ec461231165d42bb6ac3aaf
Ori Newman <orinewman1@gmail.com>
2016-08-03 12:56:40 +0300

    add properties to validateOrder

--- app/js/controllers/checkoutCtl.js
+++ app/js/controllers/checkoutCtl.js
@@ -1,5 +1,7 @@
 angular.module('app').controller('checkoutCtl', function ($uibModal, $rootScope, apiService, $filter,$scope) {
     var vm = this;
+
+    var properties = getProperties();
     $scope.tmbWidth = $rootScope.screenW*0.35;
     $rootScope.quantity = 0;
     if($scope.tmbWidth > 180){
@@ -50,20 +52,30 @@ angular.module('app').controller('checkoutCtl', function ($uibModal, $rootScope,
     vm.getSaving = function () {
         return parseFloat(vm.shipmentMethod.price) + parseFloat($rootScope.currentProduct.quantities[$rootScope.quantity].price) - vm.getTotal();
     };
+
+    vm.getDiscountPrice = function (price) {
+        return $rootScope.coupon ? price * (100 - $rootScope.coupon.shipping_discount ) / 100 : parseFloat(price);
+    };
+
     vm.checkout = function (paymentType) {
         var win = window.open('', "", "width=500, height=500");
         win.document.body.innerHTML = 'Processing...';
+        var quantity = properties.quantity.quantity;
+        properties.return_address.discountedPrice = quantity * properties.return_address.discount_price;
+        properties.return_address.totalPrice = properties.return_address.discountedPrice;
+        var returnAddressPrice = properties.return_address.totalPrice;
         return apiService
             .validateOrder(angular.extend({
                 product_id: $rootScope.currentProduct.id,
-                price: $filter('number')(vm.getDiscountProductPrice(), 2),
+                price: $filter('number')(vm.getDiscountProductPrice() + returnAddressPrice, 2),
                 curr: 'USD',
                 quantity: 1,
-                shipping_method: vm.shipmentMethod.id,
+                shipping_method: vm.shipmentMethod.method,
+                shipping_id: vm.shipmentMethod.id,
                 shipping_price: $filter('number')(vm.getDiscountShippingPrice(), 2),
                 payment_type: paymentType,
                 coupon_string: $rootScope.coupon ? $rootScope.coupon.coupon_code.replace(/'/g, '') : undefined,
-                properties:getProperties(),
+                properties:properties,
             }, $rootScope.order,{
                 country: $rootScope.order.country.code,
             }))
@@ -83,6 +95,11 @@ angular.module('app').controller('checkoutCtl', function ($uibModal, $rootScope,
                 angular.extend(property, property.pricing);
                 delete property.pricing;
             }
+            property.discount_price = parseFloat(
+                $filter('number')(
+                    vm.getDiscountPrice(property.price
+                    ),2)
+            );
             properties[param.key] = property;
         });
         return properties;
--- iframe_loader/loader.js
+++ iframe_loader/loader.js
@@ -45,7 +45,7 @@
             var offsetLeft = (screenW - iframeW) / 2;
             iframe = document.createElement('iframe');
 
-            iframe.src = 'http://localhost/pixter1/index.html?imageUrl=' + imgUrl + '&apiKey=' + apiKey +'&storeId=' + storeId + '&bgs=' + backgroundsString ;  //  add this:  #/app/sliderShop  to see the slideShop
+            iframe.src = '../index.html?imageUrl=' + imgUrl + '&apiKey=' + apiKey +'&storeId=' + storeId + '&bgs=' + backgroundsString ;  //  add this:  #/app/sliderShop  to see the slideShop
             iframe.style.position = 'fixed';
             iframe.style.left = offsetLeft + "px";
 
