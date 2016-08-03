81ab419bf0fb316eda79c7bc9b9dff72a7aefead
Ben Noach <bangoman@bens-imac.mynet>
2016-08-03 16:21:46 +0300

    .

--- iframe_loader/loader.js
+++ iframe_loader/loader.js
@@ -27,48 +27,54 @@
     function loadIframe(imgUrl,apiKey,storeId,backgrounds) {
         if (!iframe) {
             backgroundsString = encodeURIComponent(JSON.stringify(backgrounds.background))
-            overlay = document.createElement('div');
-            overlay.style.width = "100vw";
-            overlay.style.height = "100vh";
-
-
-            overlay.style.position = "fixed";
-            overlay.style.background = "rgba(0,0,0,0.4)";
-            document.body.appendChild(overlay);
-            var screenW = document.body.clientWidth;
-            var screenH = document.body.clientHeight
-            var iframeW = 800//(screenW / 100) * 64;
-            var iframeH = 628;
-            if (iframeW > 800) {
-                iframeW = 800;
-            }
-            if (screenW < 800) {
-                iframeW = screenW;
-
-            }
-
-            console.log(iframeW, screenW);
-            var offsetLeft = (screenW - iframeW) / 2;
-            iframe = document.createElement('iframe');
             var url ='http://pixter-v1-responsive.s3-website-us-east-1.amazonaws.com/index.html?imageUrl=' + imgUrl + '&apiKey=' + apiKey +'&storeId=' + storeId + '&bgs=' + backgroundsString ;  //   add this:  #/app/sliderShop  to see the slideShop
-            iframe.src = url;
             if(mobileAndTabletcheck()){
                 window.open(url,'_blank');
+            }else{
+                overlay = document.createElement('div');
+                overlay.style.width = "100vw";
+                overlay.style.height = "100vh";
+
+
+                overlay.style.position = "fixed";
+                overlay.style.background = "rgba(0,0,0,0.4)";
+                document.body.appendChild(overlay);
+                var screenW = document.body.clientWidth;
+                var screenH = document.body.clientHeight
+                var iframeW = 800//(screenW / 100) * 64;
+                var iframeH = 628;
+                if (iframeW > 800) {
+                    iframeW = 800;
+                }
+                if (screenW < 800) {
+                    iframeW = screenW;
+
+                }
+
+                console.log(iframeW, screenW);
+                var offsetLeft = (screenW - iframeW) / 2;
+                iframe = document.createElement('iframe');
+                
+                iframe.src = url;
+                iframe.style.position = 'fixed';
+                iframe.style.left = offsetLeft + "px";
+
+                iframe.style.top = (screenH - iframeH) /2;
+                iframe.style.width = iframeW + "px";
+
+                iframe.style.height = iframeH + 'px';
+                iframe.style.maxWidth = "800px";
+                iframe.style.border = "none";
+                iframe.style.borderTopLeftRadius ="15px"
+                iframe.style.borderTopRightRadius ="15px"
+                document.body.appendChild(iframe);
+
+
             }
-            iframe.style.position = 'fixed';
-            iframe.style.left = offsetLeft + "px";
 
-            iframe.style.top = (screenH - iframeH) /2;
-            iframe.style.width = iframeW + "px";
-
-            iframe.style.height = iframeH + 'px';
-            iframe.style.maxWidth = "800px";
-            iframe.style.border = "none";
-            iframe.style.borderTopLeftRadius ="15px"
-            iframe.style.borderTopRightRadius ="15px"
             
         }
-        document.body.appendChild(iframe);
+        
      //   changeImage(url);
     }
 
