600e09d42c9fcd067d6d91588aa86e817594a852
gogoyair <gogoyair@gmail.com>
2016-08-04 10:07:28 +0300

    debugging

--- app/js/controllers/checkoutCtl.js
+++ app/js/controllers/checkoutCtl.js
@@ -15,8 +15,6 @@ angular.module('app').controller('checkoutCtl', function ($uibModal, $rootScope,
         if($rootScope.currentProduct.quantities[$rootScope.quantity].pricing.shipping[key].region_id == 7){
             $scope.key = key;
         }
-             console.log("$rootScope.quantity",$rootScope.quantity);
-             console.log("$scope.key = key;",$scope.key);
              console.log("$rootScope.order",$rootScope.order);
 
         if(value.region_id == $rootScope.order.country.region.id){
