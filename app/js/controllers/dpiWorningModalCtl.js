209db3bf75bc7b33e5e854f58c595e23ff65352c
gogoyair <gogoyair@gmail.com>
2016-08-14 11:45:37 +0300

    last fixes

--- app/js/controllers/dpiWorningModalCtl.js
+++ app/js/controllers/dpiWorningModalCtl.js
@@ -1,11 +1,18 @@
 angular.module('app').controller('dpiWorningModalCtl', function ($uibModalInstance, apiService, $rootScope, $scope) {
+
     var vm = this;
 
     vm.close = $uibModalInstance.close;
     vm.dismiss = $uibModalInstance.dismiss;
     $scope.approved = function(){
         $rootScope.editToPreview = true;
-        $rootScope.dpiNotAproved = false;        
+        $rootScope.dpiNotAproved = false;
+        if($rootScope.editModeOn){
+            $rootScope.editModeOn = false;
+            $state.go('app.preview');
+        }else{
+            $state.go('app.orderDetails');
+        }        
         vm.close();
     }
 
